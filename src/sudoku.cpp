5a69de3e9a256c06269d9a3868c516586cce04eb
kutil <81024726+lukaskutac@users.noreply.github.com>
2023-01-21 15:52:15 +0100

    Add files via upload
    Starting structure of my project

---
+++ src/sudoku.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <array>
+#include "../lib/logic"
+#include "../lib/init"
+
+// -- MOTIVATION --
+// Okay, I know how this is going to sound, but hear me out...
+// I know I had some troubles in the past with staying motivated 
+// enough to finish this but this time I will finish this program, 
+// no more excuses.
+
+int main()
+{
+    int sudoku[81] = {0};
+    init::init(sudoku);
+
+    return 0;
+}
